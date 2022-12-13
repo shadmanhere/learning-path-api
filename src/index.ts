b9061c746453bb80df43335dd0f29df262078598
Shadman Ali <shadman.ali@live.co.uk>
2022-12-13 10:17:06 +0530

    nodemon and tsconfig setup

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Learning path api');
+  res.send('Learning Path Api!!!!!!!');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+        app.listen(port, () => {
+          console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+        });
