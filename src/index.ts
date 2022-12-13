e3fa9e89787bda61135684bbe1be1ea0eca7a17c
Shadman Ali <shadman.ali@live.co.uk>
2022-12-13 12:33:25 +0530

    prisma configured to use mysql, adds user model to schema.prisma

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Okay');
+  res.send('Okay 🚀');
 });
 
 app.listen(port, () => {
