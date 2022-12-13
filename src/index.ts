a009bf3a52e112b4f3be314a632ac3815f00f95a
Shadman Ali <shadman.ali@live.co.uk>
2022-12-13 10:27:12 +0530

    husky test

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-    app.get('/', (req: Request, res: Response) => {
-      res.send('Learning Path Api!!!!!!!');
-    });
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Okay');
+  });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
