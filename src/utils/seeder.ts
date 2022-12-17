83cd2b642b5b8a1c79b81b29f2a98583428d5d7a
Shadman Ali <shadman.ali@live.co.uk>
2022-12-18 05:18:10 +0530

    updates seeder to add learning path

--- src/utils/seeder.ts
+++ src/utils/seeder.ts
@@ -1,10 +1,22 @@
+import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 
-export const fetchData = async () =>
-  await axios
-    .get('https://raw.githubusercontent.com/shadmanhere/learning-path-data/main/programming-data/api/v2/tutorialsList.json')
-    .then((response) => {
-      console.log(response);
-    });
+const prisma = new PrismaClient();
 
-fetchData();
+export const fetchData = async () =>
+  await axios.get('https://raw.githubusercontent.com/shadmanhere/learning-path-data/main/programming-data/api/v2/list-of-paths.json');
+
+const createMany = async () => {
+  const { data } = await fetchData();
+  const dataObj: { name: string }[] = [];
+  data.forEach((pathName: string) => {
+    dataObj.push({ name: pathName });
+  });
+  await prisma.learningPath.deleteMany();
+  await prisma.learningPath.createMany({
+    data: dataObj,
+    skipDuplicates: true, // Skip 'Bobo'
+  });
+};
+
+createMany();
