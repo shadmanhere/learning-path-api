dcf217be812435cf9f65028c5f2e1ff0960871d6
Shadman Ali <shadman.ali@live.co.uk>
2022-12-18 09:14:35 +0530

    adds route for random tutorials

--- src/routes/tutorials.ts
+++ src/routes/tutorials.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import { getRandomTutorials } from '../controllers/tutorial';
 
 const router = express.Router();
 
+router.route('/random').get(getRandomTutorials);
 export default router;
