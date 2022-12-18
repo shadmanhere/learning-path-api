19a4b0487570577a470e67eb11909d63bd6b564a
Shadman Ali <shadman.ali@live.co.uk>
2022-12-18 09:15:38 +0530

    adds function to add tutorials form software tester learning path to database

--- src/utils/seeder.ts
+++ src/utils/seeder.ts
@@ -15,8 +15,31 @@ const createMany = async () => {
   await prisma.learningPath.deleteMany();
   await prisma.learningPath.createMany({
     data: dataObj,
-    skipDuplicates: true, // Skip 'Bobo'
+    skipDuplicates: true,
   });
 };
 
-createMany();
+// createMany();
+
+export const fetchTutorial = async () =>
+  await axios.get('https://raw.githubusercontent.com/shadmanhere/learning-path-data/main/programming-data/api/v2/software-tester.json');
+
+const updateTutorial = async () => {
+  const { data } = await fetchTutorial();
+  const tutorials: { title: string; url: string; image_url: string }[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data.forEach((dataObj: any) =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    dataObj.section.tutorials.forEach((tutorial: any) => {
+      tutorial.image_url = tutorial.thumbnail;
+      delete tutorial.thumbnail;
+      tutorials.push(tutorial);
+    }),
+  );
+  await prisma.tutorial.createMany({
+    data: tutorials,
+    skipDuplicates: true,
+  });
+};
+
+updateTutorial();
