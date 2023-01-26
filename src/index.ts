a63f3ca2b1c8f7d832556ce8e64e0242cb104d0c
Shadman Ali <shadman.ali@live.co.uk>
2023-01-26 13:34:26 +0530

    patch: adds multiple front end domain to cors

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,18 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+const allowedDomains = process.env.FRONT_END_DOMAINS?.split(' ');
 const corsOptions = {
-  origin: process.env.FRONT_END_DOMAIN,
+  origin: function (origin: string | undefined, callback: (arg0: Error | null, arg1: boolean) => any) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+
+    if (allowedDomains?.indexOf(origin) === -1) {
+      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
