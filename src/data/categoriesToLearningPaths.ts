3ce853fe1ecb465532745068ac8af69be94311da
Shadman Ali <shadman.ali@live.co.uk>
2023-01-31 05:24:45 +0530

    adds data for categoreis. kearning path and tutorials many to many relation ship

---
+++ src/data/categoriesToLearningPaths.ts
@@ -0,0 +1,8 @@
+export const categoriesToLearningPaths = [
+  // { categoryId: 1, learningPathId: 1 },
+  { categoryId: 1, learningPathId: 2 },
+  { categoryId: 1, learningPathId: 3 },
+  { categoryId: 1, learningPathId: 4 },
+  { categoryId: 1, learningPathId: 5 },
+  // { categoryId: 2, learningPathId: 6 },
+];
