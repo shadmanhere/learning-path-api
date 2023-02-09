952694227a152cf72ca045b257ef644471e2f42c
Shadman Ali <shadman.ali@live.co.uk>
2023-02-09 11:11:03 +0530

    adds new tutorial api route

--- src/routes/tutorials.ts
+++ src/routes/tutorials.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { isAuthenticated } from '../middlewares/auth';
-import { getRandomTutorials, getTutorial } from '../controllers/tutorial';
+import { authorizeRoles, isAuthenticated } from '../middlewares/auth';
+import { getRandomTutorials, getTutorial, addTutorial } from '../controllers/tutorial';
+import { Role } from '../types/user';
 
 const router = express.Router();
 
 router.route('/random').get(getRandomTutorials);
 router.route('/:videoId').get(isAuthenticated, getTutorial);
+router.route('/new').post(isAuthenticated, authorizeRoles(Role.ADMIN), addTutorial);
 export default router;
