7ab020218614b289bc860a8b144252f3a927bc2a
Shadman Ali <shadman.ali@live.co.uk>
2023-02-13 10:37:32 +0530

    adds get and delete category routes

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -7,6 +7,23 @@ import ErrorHandler from '../utils/errorHandler';
 const prisma = new PrismaClient();
 
 // Admin
+export const getCategory = catchAsyncErrors(async (req: Request, res: Response) => {
+  const resPerPage = 10;
+  const pageNumber = (req.query.pageNumber as unknown as number) || 1;
+  const totalCount = await prisma.category.count({});
+  const categories = await prisma.category.findMany({
+    skip: (pageNumber - 1) * resPerPage,
+    take: +resPerPage,
+  });
+  res.status(200).json({
+    success: true,
+    totalCount,
+    resPerPage,
+    pageNumber,
+    categories,
+  });
+});
+
 export const addCategory = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
   if (!name) {
@@ -41,5 +58,24 @@ export const updateCategory = catchAsyncErrors(async (req: Request, res: Respons
   res.send({
     success: true,
     category,
+  });
+});
+
+export const deleteCategory = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return next(new ErrorHandler('Please enter id', 400));
+  }
+
+  const deleteCategory = await prisma.category.delete({
+    where: {
+      id: +id,
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    deleteCategory,
   });
 });
--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authorizeRoles, isAuthenticated } from '../middlewares/auth';
-import { addCategory, updateCategory } from '../controllers/category';
+import { addCategory, updateCategory, getCategory, deleteCategory } from '../controllers/category';
 import { Role } from '../types/user';
 
 const router = express.Router();
 
+router.route('/get').get(isAuthenticated, authorizeRoles(Role.ADMIN), getCategory);
 router.route('/new').post(isAuthenticated, authorizeRoles(Role.ADMIN), addCategory);
 router.route('/update').put(isAuthenticated, authorizeRoles(Role.ADMIN), updateCategory);
+router.route('/delete/:id').delete(isAuthenticated, authorizeRoles(Role.ADMIN), deleteCategory);
 
 export default router;
