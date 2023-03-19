0039fb511c7120b61d9ed333f57408fd98dad04c
Shadman Ali <shadman.ali@live.co.uk>
2023-03-19 09:13:37 +0530

    adds chapters route

--- src/routes/chapters.ts
+++ src/routes/chapters.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { authorizeRoles, isAuthenticated } from '../middlewares/auth';
-import { getChapters } from '../controllers/chapters';
+import { getChapters, updateChapter, addChapter, deleteChapter } from '../controllers/chapters';
 import { Role } from '../types/user';
 
 const router = express.Router();
 
 router.route('/').get(isAuthenticated, getChapters);
+router.route('/new').post(isAuthenticated, authorizeRoles(Role.ADMIN), addChapter);
+router.route('/update').put(isAuthenticated, authorizeRoles(Role.ADMIN), updateChapter);
+router.route('/delete/:id').delete(isAuthenticated, authorizeRoles(Role.ADMIN), deleteChapter);
 
 export default router;
