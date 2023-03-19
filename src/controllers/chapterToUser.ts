37c8dcbd75d1da463ea8f5369129385783c4bc30
Shadman Ali <shadman.ali@live.co.uk>
2023-03-19 10:01:07 +0530

    adds update chapterToUser controller

--- src/controllers/chapterToUser.ts
+++ src/controllers/chapterToUser.ts
@@ -6,6 +6,7 @@ import ErrorHandler from '../utils/errorHandler';
 
 const prisma = new PrismaClient();
 
+// Admin
 export const addChapterToUser = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
   const { chapterId, userId } = req.body;
   if (!chapterId) {
@@ -24,5 +25,32 @@ export const addChapterToUser = catchAsyncErrors(async (req: Request, res: Respo
   res.send({
     success: true,
     chapterToUser,
+  });
+});
+
+export const updateChapterToUser = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+  const { chapterId, userId, newUserId, newChapterId } = req.body;
+  if (!chapterId) {
+    return next(new ErrorHandler('Please enter chapter id', 400));
+  }
+  if (!userId) {
+    return next(new ErrorHandler('Please enter user id', 400));
+  }
+
+  const updatedChapterToUser = await prisma.chapterToUser.update({
+    where: {
+      chapterId_userId: {
+        chapterId: +chapterId,
+        userId: +userId,
+      },
+    },
+    data: {
+      chapterId: +chapterId,
+      userId: +userId,
+    },
+  });
+  res.send({
+    success: true,
+    updatedChapterToUser,
   });
 });
